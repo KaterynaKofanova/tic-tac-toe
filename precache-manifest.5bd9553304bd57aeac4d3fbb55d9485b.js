81f21272d032acc89dc0126af42e5300dfc0587b
KaterynaKofanova <katerynakofanova@gmail.com>
2020-09-25 15:26:53 +0300

    Updates

---
+++ precache-manifest.5bd9553304bd57aeac4d3fbb55d9485b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c41aef6cc0735e3a694966b12d0c85",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a0fcdd78865dc36edce1",
+    "url": "/tic-tac-toe/static/css/main.fe78d11c.chunk.css"
+  },
+  {
+    "revision": "ad8e231a0a54f8d12802",
+    "url": "/tic-tac-toe/static/js/2.643dab68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.643dab68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fcdd78865dc36edce1",
+    "url": "/tic-tac-toe/static/js/main.4ca9d31a.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
